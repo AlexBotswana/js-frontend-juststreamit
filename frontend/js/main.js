45f2f28cec3ce8d77eccfdc18f5f7b1e726c21ce
AlexBotswana <alfacama@outlook.fr>
2023-02-13 23:26:33 +0200

    modal 1

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,7 +12,8 @@ function fetchBestMovie() {
 			imageHtml.src = imageUrl
 			let titleBestMovie = document.getElementById("title-best-movie-top")
 			titleBestMovie.textContent = bestMovie.title
-			console.log(titleBestMovie)
+			let titleBestMovieDetails = document.getElementById("title-best-movie-top-d")
+			titleBestMovieDetails.textContent = bestMovie.title
 		});
 }
 
@@ -80,12 +81,38 @@ function fetchSevenBestSciFiMovies() {
 		});
 }
 
+function modal() {
+	// Get the modal
+ 	var modal = document.getElementById("myModal");
+  	// Get the button that opens the modal
+  	var btn = document.getElementById("myBtn");
+  	// Get the <span> element that closes the modal
+  	var span = document.getElementsByClassName("close")[0];
+  	// When the user clicks on the button, open the modal
+  	btn.onclick = function() {
+  	modal.style.display = "block";
+  	}
+  	// When the user clicks on <span> (x), close the modal
+  	span.onclick = function() {
+	modal.style.display = "none";
+  	}
+  	// When the user clicks anywhere outside of the modal, close it
+  	window.onclick = function(event) {
+  	  if (event.target == modal) {
+      modal.style.display = "none";
+  	  }
+  	} 
+}
+
 function main() {
 	fetchBestMovie();
 	fetchSevenBestMovies();
 	fetchSevenBestActionMovies();
 	fetchSevenBestComedyMovies();
 	fetchSevenBestSciFiMovies();
+	modal();
 }
+
+
 
 main();
